a81442a61d695c686816752731a7d47fa583769e
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-05-30 20:09:12 -0600

    Oh my zsh!

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,8 @@
+# My aliases
+alias lss='tree -L 1'
+alias pyserve='python3 -m http.server'
+alias bat='batcat --theme=gruvbox-dark'
+alias fd='fdfind'
+alias check_bat='upower -i $(upower -e | rg "BAT")'
+alias exa='exa --icons'
+
