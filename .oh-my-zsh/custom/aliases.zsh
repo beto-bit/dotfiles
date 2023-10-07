9b94541fe80b3506a885f4d58b905138737cb1e1
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-10-06 18:28:58 -0600

    Bunch of changes

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,5 +5,5 @@ alias bat='batcat'
 alias fd='fdfind'
 alias check_bat='upower -i $(upower -e | rg "BAT")'
 alias exa='exa --icons'
-alias checkass='f() {objdump -dC -M intel $1 | bat -l asm};f'
+alias checkass='f() {objdump -dC -M intel $1 | bat -l asm --wrap=never};f'
 
