ebea706b6c36bbdc59c96a1dc4b248406e97b1e0
beto-bit <76757367+beto-bit@users.noreply.github.com>
2025-03-09 13:38:05 -0600

    Batcat theme and extra script

---
+++ maintenance.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+printf "Checking for failed systemd services...\n\n"
+systemctl --failed
+
+printf "Updating everything...\n\n"
+yay -Syu
+
+printf "Removing orphan packages...\n\n"
+pacman -Qdtq | rg --invert-match ".-debug" | pacman -Rns -
+
